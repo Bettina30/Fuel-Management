cf61a92e423843aba015cae72421a7bd110a2969
Bettina30 <bettinathomas30@gmail.com>
2024-02-14 23:19:01 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");  
+error_reporting(0);  
+session_start(); 
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>Home</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"> </head>
+
+<body class="home">
+    
+        <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/tick.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="petrolpump.php">petrolpump<span class="sr-only"></span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+
+        <section class="hero bg-image" data-image-src="images/img/index1.jpg">
+            <div class="hero-inner">
+                <div class="container text-center hero-text font-white">
+                    
+                    
+                    <div class="banner-form">
+                        <form class="form-inline">
+                          
+                        </form>
+                    </div>
+                    
+
+
+                    <div class="step-item step3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewbox="0 0 612.001 612">
+                                <path d="M604.131 440.17h-19.12V333.237c0-12.512-3.776-24.787-10.78-35.173l-47.92-70.975a62.99 62.99 0 0 0-52.169-27.698h-74.28c-8.734 0-15.737 7.082-15.737 15.738v225.043h-121.65c11.567 9.992 19.514 23.92 21.796 39.658H412.53c4.563-31.238 31.475-55.396 63.972-55.396 32.498 0 59.33 24.158 63.895 55.396h63.735c4.328 0 7.869-3.541 7.869-7.869V448.04c-.001-4.327-3.541-7.87-7.87-7.87zM525.76 312.227h-98.044a7.842 7.842 0 0 1-7.868-7.869v-54.372c0-4.328 3.541-7.869 7.868-7.869h59.724c2.597 0 4.957 1.259 6.452 3.305l38.32 54.451c3.619 5.194-.079 12.354-6.452 12.354zM476.502 440.17c-27.068 0-48.943 21.953-48.943 49.021 0 26.99 21.875 48.943 48.943 48.943 26.989 0 48.943-21.953 48.943-48.943 0-27.066-21.954-49.021-48.943-49.021zm0 73.495c-13.535 0-24.472-11.016-24.472-24.471 0-13.535 10.937-24.473 24.472-24.473 13.533 0 24.472 10.938 24.472 24.473 0 13.455-10.938 24.471-24.472 24.471zM68.434 440.17c-4.328 0-7.869 3.543-7.869 7.869v23.922c0 4.328 3.541 7.869 7.869 7.869h87.971c2.282-15.738 10.229-29.666 21.718-39.658H68.434v-.002zm151.864 0c-26.989 0-48.943 21.953-48.943 49.021 0 26.99 21.954 48.943 48.943 48.943 27.068 0 48.943-21.953 48.943-48.943.001-27.066-21.874-49.021-48.943-49.021zm0 73.495c-13.534 0-24.471-11.016-24.471-24.471 0-13.535 10.937-24.473 24.471-24.473s24.472 10.938 24.472 24.473c0 13.455-10.938 24.471-24.472 24.471zm117.716-363.06h-91.198c4.485 13.298 6.846 27.54 6.846 42.255 0 74.28-60.431 134.711-134.711 134.711-13.535 0-26.675-2.045-39.029-5.744v86.949c0 4.328 3.541 7.869 7.869 7.869h265.96c4.329 0 7.869-3.541 7.869-7.869V174.211c-.001-13.062-10.545-23.606-23.606-23.606zM118.969 73.866C53.264 73.866 0 127.129 0 192.834s53.264 118.969 118.969 118.969 118.97-53.264 118.97-118.969-53.265-118.968-118.97-118.968zm0 210.864c-50.752 0-91.896-41.143-91.896-91.896s41.144-91.896 91.896-91.896c50.753 0 91.896 41.144 91.896 91.896 0 50.753-41.143 91.896-91.896 91.896zm35.097-72.488c-1.014 0-2.052-.131-3.082-.407L112.641 201.5a11.808 11.808 0 0 1-8.729-11.396v-59.015c0-6.516 5.287-11.803 11.803-11.803 6.516 0 11.803 5.287 11.803 11.803v49.971l29.614 7.983c6.294 1.698 10.02 8.177 8.322 14.469-1.421 5.264-6.185 8.73-11.388 8.73z" fill="#FFF" /> </svg>
+                            <h4><span style="color:white;"></span>Delivery or take out</h4> </div>
+                   
+                    </div>
+      
+        </section>
+      
+      
+	  
+	
+     
+        <section class="popular">
+            <div class="container">
+                <div class="title text-xs-center m-b-30">
+                    <h2>Popular items of the Month</h2>
+                    <p class="lead">Easiest way to order your Fuels</p>
+                </div>
+                <div class="row">
+						<?php 					
+						$query_res= mysqli_query($db,"select * from items LIMIT 6"); 
+                                while($r=mysqli_fetch_array($query_res))
+                                {
+                                        
+                                    echo '  <div class="col-xs-12 col-sm-6 col-md-4 food-item">
+                                            <div class="food-item-wrap">
+                                                <div class="figure-wrap bg-image" data-image-src="admin/Res_img/items/'.$r['img'].'"></div>
+                                                <div class="content">
+                                                    <h5><a href="items.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
+                                                    <div class="product-name">'.$r['slogan'].'</div>
+                                                    <div class="price-btn-block"> <span class="price">₹'.$r['price'].'</span> <a href="items.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
+                                                </div>
+                                                
+                                            </div>
+                                    </div>';                                      
+                                }	
+						?>
+                </div>
+            </div>
+        </section>
+ 
+        <section class="how-it-works">
+            <div class="container">
+                <div class="text-xs-center">
+                    <h2>Easy to Order</h2>
+                    <div class="row how-it-works-solution">
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col1">
+                            <div class="how-it-works-wrap">
+                                <div class="step step-1">
+                                    <div class="icon" data-step="1">
+                                        
+
+
+                                    </div>
+                                    <h3>Choose a petrolpump</h3>
+                                    <p>We"ve got your covered with items from a variety of delivery petrolpump online.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col2">
+                            <div class="step step-2">
+                                <div class="icon" data-step="2">
+                                    
+                                </div>
+                                <h3>Choose a item</h3>
+                                <p>We"ve got your covered with a variety of delivery petrolpump online.</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col3">
+                            <div class="step step-3">
+                                <div class="icon" data-step="3">
+                                    
+                                </div>
+                                <h3>Pick up or Delivery</h3>
+                                <p>Get your fuel delivered! And enjoy your Ride! </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        <p class="pay-info">Cash on Delivery</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="featured-petrolpump">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="title-block pull-left">
+                            <h3>Featured petrolpump</h3> </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="petrolpump-filter pull-right">
+                            <nav class="primary pull-left">
+                                <ul>
+                                <li><a href="petrolpump.php" class="selected" data-filter="*">all</a> </li>
+									
+                                <?php 
+									$res= mysqli_query($db,"select * from res_category");
+									      while($row=mysqli_fetch_array($res))
+										  {
+											echo '<li><a href="#" data-filter=".'.$row['c_name'].'"> '.$row['c_name'].'</a> </li>';
+										  }
+									?>
+                                   
+                                </ul>
+                            </nav>
+                        </div>
+          
+                    </div>
+                </div>
+               
+            </div>
+        </section>
+        
+      
+        <footer class="footer">
+            <div class="container">
+                
+          
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>Payment Options</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5>Address</h5>
+                                    <p>WhiteField,Bangalore</p>
+                                    <h5>Phone: 8151866928,9961852709</a></h5> </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5>About Us</h5>
+                                   <p>The Fuel Delivery (TFD) is an endeavor to largely solve the issues of managing your fuel requirement efficiently, cost-effectively & most importantly, safely. We deliver diesel to users in the manner prescribed by the petroleum ministry and we are licensed by PESO (Petroleum & Explosives Safety Organization). Subject to government regulations we strive to revolutionize fuel delivery in India, by transporting diesel at your doorstep.
+
+.</p>
+                                </div>
+                    </div>
+                </div>
+          
+            </div>
+        </footer>
+    
+    
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
